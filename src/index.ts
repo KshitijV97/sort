9d7576792689fc6366945abf1c15b55f5dc82915
KshitijV97 <writetokshitij@gmail.com>
2022-03-05 21:35:21 +0530

    Sorting array, string and linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
 const numbersCollection = new NumbersCollection([1000, -5, 3, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('zyxa');
+charactersCollection.sort();
+console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(3);
+linkedList.add(2);
+linkedList.add(1);
+linkedList.sort();
+linkedList.print();
