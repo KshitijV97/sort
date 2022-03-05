67ff9cbae79cbefdbf950a1a5ba5b635ff30f24c
KshitijV97 <writetokshitij@gmail.com>
2022-03-05 21:29:49 +0530

    Placing sort call

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,5 @@ import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
 const numbersCollection = new NumbersCollection([1000, -5, 3, 0]);
-
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log('NumbersCollection.data is', numbersCollection.data);
-console.log('Sorted collection is', sorter.collection);
+numbersCollection.sort();
+console.log(numbersCollection.data);
